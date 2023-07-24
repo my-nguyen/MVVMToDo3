0001c8becb055c654a64f232a3cb37d1209e8412
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-07-23 18:08:39 -0700

    b2: Layouts & Room Entity

---
+++ app/src/main/java/com/nguyen/mvvmtodo3/Task.kt
@@ -0,0 +1,20 @@
+package com.nguyen.mvvmtodo3
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "tasks")
+@Parcelize
+data class Task(
+    val name: String,
+    val important: Boolean = false,
+    val completed: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+): Parcelable {
+    val createdFormatted: String
+        get() = DateFormat.getDateInstance().format(created)
+}
